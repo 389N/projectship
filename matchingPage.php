f88d4002158512daf0eb70de6df16d6da713a984
Kevin Feng <kfeng64@gmail.com>
2017-12-01 05:47:39 -0500

    matching with filter

--- matchingPage.php
+++ matchingPage.php
@@ -5,6 +5,74 @@
 		session_start();
 	}
 
+	$filterM = "checked=checked";
+	$filterF = "checked=checked";
+	$filterCS = "checked=checked";
+	$filterBS = "checked=checked";
+	$filterEN = "checked=checked";
+	$filterSr = "checked=checked";
+	$filterJr = "checked=checked";
+	$filterSp = "checked=checked";
+	$filterFr = "checked=checked";
+
+	$queryFilter = "";
+
+	if (isset($_POST['gender'])) {
+		if (!in_array("Male", $_POST['gender'])) {
+			$filterM = "";
+		}
+		if (!in_array("Female", $_POST['gender'])) {
+			$filterF = "";
+		}
+
+		if (getFilter($_POST['gender'], "gender") != "") {
+			$queryFilter .= "(".getFilter($_POST['gender'], "gender").")";
+		}
+	}
+
+	if (isset($_POST['major'])) {
+		if (!in_array("Computer Science", $_POST['major'])) {
+			$filterCS = "";
+		}
+		if (!in_array("Business", $_POST['major'])) {
+			$filterBS = "";
+		}
+		if (!in_array("Engineering", $_POST['major'])) {
+			$filterEN = "";
+		}
+
+		if (getFilter($_POST['major'], "major") != "" && getFilter($_POST['gender'], "gender") != "") {
+			$queryFilter .= " and ";
+		}
+
+		if (getFilter($_POST['major'], "major") != "") {
+			$queryFilter .= "(".getFilter($_POST['major'], "major").")";
+		}
+	}
+
+	if (isset($_POST['grade'])) {
+		if (!in_array("Senior", $_POST['grade'])) {
+			$filterSr = "";
+		}
+		if (!in_array("Junior", $_POST['grade'])) {
+			$filterJr = "";
+		}
+		if (!in_array("Sophomore", $_POST['grade'])) {
+			$filterSp = "";
+		}
+		if (!in_array("Freshman", $_POST['grade'])) {
+			$filterFr = "";
+		}
+
+		if ($queryFilter != "" && getFilter($_POST['grade'], "grade") != "") {
+			$queryFilter .= " and ";
+		}
+		if (getFilter($_POST['grade'], "grade") != "") {
+			$queryFilter .= "(".getFilter($_POST['grade'], "grade").")";
+		}
+		
+	}
+
 	$host = "localhost";
 	$user = "dbuser";
 	$dbpassword = "goodbyeWorld";
@@ -22,7 +90,12 @@
 	$profilesArray = [];
 	$usersArray = [];
 
-	$query = "select * from $table where email != \"$currUser\"";
+	if ($queryFilter != "") {
+		$query = "select * from $table where email != \"$currUser\" and ($queryFilter)";
+	} else {
+		$query = "select * from $table where email != \"$currUser\"";
+	}
+	
 	$result = $db_connection->query($query);
 	if ($result) {
 		$num_rows = $result->num_rows;
@@ -53,46 +126,47 @@
 		die("Retrieval failed: ". $db_connection->error);
 	}
 
-	$currProfile = $usersArray[0];
-
 	$userEmailsJSON = json_encode($usersArray);
 	$profilesJSON = json_encode($profilesArray);
 
-	//print_r($profilesArray);
-
-
-
 	$body = <<<BODY
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<style>
-		.card {
-		  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-		  max-width: 500px;
-		  margin: auto;
-		  text-align: center;
-		  font-family: arial;
-		  background-color: white;
-		}
 
-		button {
-		  border: none;
-		  outline: 0;
-		  display: inline-block;
-		  padding: 8px;
-		  color: white;
-		  background-color: #000;
-		  text-align: center;
-		  cursor: pointer;
-		  width: 100%;
-		  font-size: 18px;
-		}
+		<center><h1> Here are the "Matched" friends! </h1>
 
-		button:hover, a:hover {
-		  opacity: 0.7;
-		}
-		</style>
+		<form action="{$_SERVER['PHP_SELF']}" method="post">
+			<strong>Filter:</strong><br>
 
-		<center><h1> Here are the "Matched" friends! </h1><br>
+			<!--SOMEONE MAKE THIS LOOK PRETTY-->
+
+			<span class="filter">Gender: <br> 
+			<input type="checkbox" name="gender[]" value="Male" $filterM>Male <br>
+			<input type="checkbox" name="gender[]" value="Female" $filterF>Female <br>
+			<input type="hidden" name="gender[]" value="">
+			<br><br>
+			</span>
+
+			<span class="filter">Major: <br>
+			<input type="checkbox" name="major[]" value="Computer Science" $filterCS>Computer Science <br>
+			<input type="checkbox" name="major[]" value="Business" $filterBS>Business <br>
+			<input type="checkbox" name="major[]" value="Engineering" $filterEN>Engineering <br>
+			<input type="hidden" name="major[]" value="">
+			<br>
+			</span>
+
+			<span class="filter">Grade: <br>
+			<input type="checkbox" name="grade[]" value="Senior" $filterSr>Senior <br>
+			<input type="checkbox" name="grade[]" value="Junior" $filterJr>Junior <br>
+			<input type="checkbox" name="grade[]" value="Sophomore" $filterSp>Sophomore <br>
+			<input type="checkbox" name="grade[]" value="Freshman" $filterFr>Freshman <br>
+			<input type="hidden" name="grade[]" value="">
+			</span>
+
+			<br>
+
+			<input type="submit" name="submit" value="Filter Results">
+		</form>
+		<br>
 
 		<input type="button" id="prev" value="Previous Match">
 		<input type="button" id="next" value="Next Match">
@@ -100,13 +174,9 @@
 
 		<div class="card">
 			<div id="profile">
-			$profilesArray[$currProfile]
 			</div>
 		 	<p><button>Contact</button></p>
-		</div>
-		
-
-		
+		</div>	
 
 		<input type="button" onclick="location.href='homePage.php';" value="Home Page" /> </center>
 
@@ -120,12 +190,18 @@
 			console.log(profiles);
 			console.log(emails);
 
+			var noMatch = "<h1>Sorry!</h1><br><h2>We couldn't find any matches for you! </h2><h3>(Try changing your filter settings)</h3>"
+
+			if (profiles.length == 0) {
+				document.getElementById("profile").innerHTML = noMatch;
+			} else {
+				document.getElementById("profile").innerHTML = profiles[emails[profileIndex]];
+			}
 
 			document.getElementById("next").addEventListener("click", function() {
 				if (profileIndex < emails.length - 1) {
 					profileIndex++;
-					document.getElementById("profile").innerHTML = profiles[emails[profileIndex]];
-					
+					document.getElementById("profile").innerHTML = profiles[emails[profileIndex]];	
 				}
 			});
 
@@ -136,14 +212,22 @@
 				}
 			});
 
-
-
 		</script>
 
-
-
 BODY;
-	
+
+function getFilter($postArr, $category) {
+	$filters = "";
+
+	for ($i = 0; $i < count($postArr) - 1; $i++) {
+		if ($i == count($postArr) - 2) {
+			$filters .= "$category = \"$postArr[$i]\" ";
+		} else {
+			$filters .= "$category = \"$postArr[$i]\" or ";
+		}
+	}
+	return $filters;
+}
 
 	$db_connection->close();
 
