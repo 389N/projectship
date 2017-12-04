f9c5f009b3f9e6ae52f80b12fcab0988fd0dc267
Kevin Feng <kfeng64@gmail.com>
2017-12-04 06:33:39 -0500

    finished

--- matchingPage.php
+++ matchingPage.php
@@ -215,7 +215,17 @@
 		<div class="card">
 			<div id="profile">
 			</div>
-		 	<p><button>Contact</button></p>
+		 	<p><button id="contact">Contact</button></p>
+		 	<!-- The Modal -->
+			<div id="myModal" class="modal">
+
+			  <!-- Modal content -->
+			  <div class="modal-content">
+			    <span class="close">&times;</span>
+			    <p><h2>User has been contacted!</h2></p>
+			  </div>
+
+			</div>
 		</div>	
 		<input type="button" onclick="location.href='homePage.php';" class="btn btn-default" value="Home Page" /> </center>
 		<br>
@@ -277,6 +287,35 @@
 					document.getElementById("profile").innerHTML = profiles[emails[profileIndex]];
 				}
 			});
+
+
+			// Get the modal
+			var modal = document.getElementById('myModal');
+
+			// Get the button that opens the modal
+			var btn = document.getElementById("contact");
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks the button, open the modal 
+			btn.onclick = function() {
+			    modal.style.display = "block";
+			}
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+			    modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+			    if (event.target == modal) {
+			        modal.style.display = "none";
+			    }
+			}
+
+
 
 		</script>
 
